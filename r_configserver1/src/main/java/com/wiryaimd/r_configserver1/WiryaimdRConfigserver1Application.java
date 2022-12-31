165a04b7dcd32daebd535554fc6c36e617901ee9
wiryaimd <adnyasutha003@gmail.com>
2022-12-31 16:31:38 +0800

    config limit service

--- r_configserver1/src/main/java/com/wiryaimd/r_configserver1/WiryaimdRConfigserver1Application.java
+++ r_configserver1/src/main/java/com/wiryaimd/r_configserver1/WiryaimdRConfigserver1Application.java
@@ -2,7 +2,9 @@ package com.wiryaimd.r_configserver1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer // agar dapat mengakses endpoint file konfi properties yg ada pada git-local-config
 @SpringBootApplication
 public class WiryaimdRConfigserver1Application {
 
