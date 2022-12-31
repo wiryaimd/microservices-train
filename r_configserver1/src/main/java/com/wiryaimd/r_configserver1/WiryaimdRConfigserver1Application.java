9569920d5e048bce8032eec39e374f8b8844b95a
wiryaimd <adnyasutha003@gmail.com>
2022-12-31 15:37:32 +0800

    setup global config

---
+++ r_configserver1/src/main/java/com/wiryaimd/r_configserver1/WiryaimdRConfigserver1Application.java
@@ -0,0 +1,13 @@
+package com.wiryaimd.r_configserver1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiryaimdRConfigserver1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiryaimdRConfigserver1Application.class, args);
+	}
+
+}
---
+++ r_configserver1/src/test/java/com/wiryaimd/r_configserver1/WiryaimdRConfigserver1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiryaimd.r_configserver1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiryaimdRConfigserver1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
