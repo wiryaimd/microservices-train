8fe6e3d5324784a12a9e5a15abd2d255409967ea
wiryaimd <adnyasutha003@gmail.com>
2023-01-01 12:59:15 +0800

    add currency exchange service

---
+++ r_services2-currencyexchange/src/main/java/com/wiryaimd/r_services2currencyexchange/RServices2CurrencyexchangeApplication.java
@@ -0,0 +1,13 @@
+package com.wiryaimd.r_services2currencyexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RServices2CurrencyexchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RServices2CurrencyexchangeApplication.class, args);
+	}
+
+}
---
+++ r_services2-currencyexchange/src/test/java/com/wiryaimd/r_services2currencyexchange/RServices2CurrencyexchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiryaimd.r_services2currencyexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RServices2CurrencyexchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
