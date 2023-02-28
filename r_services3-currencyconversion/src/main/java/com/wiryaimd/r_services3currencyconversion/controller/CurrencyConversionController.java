6ead55184da6edc2d60c492084749f1cdb81dc2f
wiryaimd <adnyasutha003@gmail.com>
2023-02-28 20:48:44 +0800

    conversion controller

--- r_services3-currencyconversion/src/main/java/com/wiryaimd/r_services3currencyconversion/controller/CurrencyConversionController.java
+++ r_services3-currencyconversion/src/main/java/com/wiryaimd/r_services3currencyconversion/controller/CurrencyConversionController.java
@@ -2,16 +2,21 @@ package com.wiryaimd.r_services3currencyconversion.controller;
 
 import com.wiryaimd.r_services3currencyconversion.model.CurrencyConversionModel;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @AllArgsConstructor
+@Slf4j
 public class CurrencyConversionController {
 
     private Environment environment;
@@ -19,7 +24,22 @@ public class CurrencyConversionController {
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public ResponseEntity<CurrencyConversionModel> calculateCurrencyConversion(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity){
 
-        return ResponseEntity.ok(new CurrencyConversionModel(1000L, from, to, BigDecimal.ONE, quantity, BigDecimal.TEN, environment.getProperty("local.server.port")));
+        Map<String, String> map = new HashMap<>();
+        map.put("from", from);
+        map.put("to", to);
+
+        ResponseEntity<CurrencyConversionModel> responseEntity = new RestTemplate().getForEntity(
+                "http://localhost:8002/currency-exchange/from/{from}/to/{to}",
+                CurrencyConversionModel.class,
+                map
+        );
+        CurrencyConversionModel currencyConversionModel = responseEntity.getBody();
+
+        if (currencyConversionModel == null){
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(new CurrencyConversionModel(currencyConversionModel.getId(), from, to, currencyConversionModel.getConversionMultiple(), quantity, quantity.multiply(currencyConversionModel.getConversionMultiple()), currencyConversionModel.getEnvironment()));
     }
 
 }
